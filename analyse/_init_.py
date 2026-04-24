a61450b26c687f4124010e3e466dbadda083d3c4
bakwonken-hub <bakwonken@gmail.com>
2026-04-24 17:27:51 +0100

    Update exports in init.py for new modules

---
+++ analyse/_init_.py
@@ -0,0 +1,4 @@
+from .stats import AnalyseEcommerce
+from .alertes import SystemeAlertes
+
+__all__ = ["AnalyseEcommerce", "SystemeAlertes"]
--- analyse/init.py
+++
@@ -1,5 +0,0 @@
-from .amazon import CollecteurAmazon
-from .fnac import CollecteurFnac
-from .darty import CollecteurDarty
-
-__all__ = ["CollecteurAmazon", "CollecteurFnac", "CollecteurDarty"]
